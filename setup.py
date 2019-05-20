1b26f76f6d012f391c3408c33f512074f0605015
lucky-singh <luckysingh223@gmail.com>
2019-05-20 17:25:52 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='pgpackup',
+    version='0.1.0',
+    author='Lucky Singh',
+    author_email='luckysingh223@gmail.com',
+    description='A utility for backing up PostgreSQL databases',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/lucky-singh/pgbackup',
+    packages=find_packages('src')
+)
